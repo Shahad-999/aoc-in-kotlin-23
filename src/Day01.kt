32d8682e33b03f4327395f473c6c05af19030c7a
Shahad-999 <shahad.h.999.a@gmail.com>
2023-12-01 21:53:50 +0300

    Day01 👏👏👏

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line->
+            line.filter { it.isDigit() }.run {
+                combineFirstAndLastAsNum()
+            }
+        }
     }
-
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf {line->
+            val digits = StringBuilder()
+            line.forEachIndexed { index ,char ->
+                digits.append(getFirstNumber(line,char,index))
+            }
+            digits.toString().combineFirstAndLastAsNum()
+        }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
+    
+    
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+    
+}
+fun getFirstNumber(text: String, firstDigit: Char,currentIndex:Int): String {
+    if(firstDigit.isDigit()) {
+        return "$firstDigit"
+    }else{
+        getNumberOrNull(text.substring(currentIndex))?.let {number->
+            return "$number"
+        }
+    }
+    return ""
+}
+
+val numbers = listOf("one", "two", "three", "four", "five", "six", "seven", "eight","nine" )
+fun getNumberOrNull(text: String): Int?{
+    numbers.forEachIndexed { index, num ->
+        if(text.take(num.length).contains(num)) return  index+1
+    }
+    return null
+}
+fun String.combineFirstAndLastAsNum(): Int {
+    return "${first()}${last()}".toInt()
 }
